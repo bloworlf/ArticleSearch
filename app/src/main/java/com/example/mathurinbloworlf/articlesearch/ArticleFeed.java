30d0ca4fecf51a6c3f0244af812c9d9776461292
Mathurin Bloworlf <blow.mathurin@gmail.com>
2018-07-11 00:29:00 -0400

    Initial commit: project creation

---
+++ app/src/main/java/com/example/mathurinbloworlf/articlesearch/ArticleFeed.java
@@ -0,0 +1,13 @@
+package com.example.mathurinbloworlf.articlesearch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ArticleFeed extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_article_feed);
+    }
+}
